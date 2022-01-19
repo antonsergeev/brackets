a810d88e4f0eff9847e0cd62fbb3e7f88a342542
Anton Sergeev <antonsergeevphd@gmail.com>
2022-01-20 00:29:37 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const regex = new RegExp(bracketsConfig.map(brackets => '\\' + brackets.join('\\')).join('|'), 'g');
+  let testStr = str;
+  while (regex.test(testStr)) {
+    testStr = testStr.replace(regex, '');
+  }
+  return testStr.length === 0;
 }
